cdb0aef1d203731a71a4cd662189df83a1a901ba
julitran <juliee.trann@gmail.com>
2022-01-11 21:06:03 -0700

    initial commit message

---
+++ src/com/julitran/Main.java
@@ -0,0 +1,12 @@
+package com.julitran;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 welcomeToMyProgram();
+    }
+
+    private static void welcomeToMyProgram() {
+        System.out.print("Hello World");
+    }
+}
