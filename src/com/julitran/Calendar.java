1044213f454d42e4d9afcc863d153c2957defb90
julitran <juliee.trann@gmail.com>
2022-01-11 22:00:25 -0700

    created Calendar

---
+++ src/com/julitran/Calendar.java
@@ -0,0 +1,18 @@
+package com.julitran;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calendar {
+
+    List calendar = new ArrayList();
+
+
+    public List getCalendar() {
+        return calendar;
+    }
+
+    public void setCalendar(List calendar) {
+        this.calendar = calendar;
+    }
+}
--- src/com/julitran/Main.java
+++ src/com/julitran/Main.java
@@ -1,5 +1,8 @@
 package com.julitran;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,7 +10,21 @@ public class Main {
     }
 
     private static void welcomeToMyProgram() {
-        System.out.print("Hello World");
+        List calendar = new ArrayList();
+        calendar.add("January");
+        calendar.add("February");
+        calendar.add("March");
+        calendar.add("April");
+        calendar.add("May");
+        calendar.add("June");
+        calendar.add("July");
+        calendar.add("August");
+        calendar.add("September");
+        calendar.add("October");
+        calendar.add("November");
+        calendar.add("December");
+
+        calendar.stream().forEach((element) -> System.out.println(element));
     }
 
 }
