309418dfeb517dd0787a7f800147612625deaf05
julitran <juliee.trann@gmail.com>
2022-04-20 11:58:24 -0600

    changed Calendar as HashSet

--- src/com/julitran/Main.java
+++ src/com/julitran/Main.java
@@ -1,6 +1,7 @@
 package com.julitran;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
@@ -8,25 +9,24 @@ public class Main {
     public static void main(String[] args) {
 
         Main main = new Main();
-        List calendar = main.welcomeToMyProgram();
+        HashMap calendar = main.getCalendar();
+        System.out.print(calendar.get(1));
     }
 
-    private List welcomeToMyProgram() {
-        List calendar = new ArrayList();
-        calendar.add("January");
-        calendar.add("February");
-        calendar.add("March");
-        calendar.add("April");
-        calendar.add("May");
-        calendar.add("June");
-        calendar.add("July");
-        calendar.add("August");
-        calendar.add("September");
-        calendar.add("October");
-        calendar.add("November");
-        calendar.add("December");
-
-        calendar.stream().forEach((element) -> System.out.println(element));
+    private HashMap getCalendar() {
+        HashMap<Integer, String> calendar = new HashMap<>();
+        calendar.put(1, "January");
+        calendar.put(2, "February");
+        calendar.put(3, "March");
+        calendar.put(4, "April");
+        calendar.put(5, "May");
+        calendar.put(6, "June");
+        calendar.put(7, "July");
+        calendar.put(8, "August");
+        calendar.put(9, "September");
+        calendar.put(10, "October");
+        calendar.put(11, "November");
+        calendar.put(12, "December");
         return calendar;
     }
 
